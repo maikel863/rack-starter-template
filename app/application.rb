ac38d2324aad563d4a126f3b0ead9a8e6757baf9
Maikel <maikel863@outlook.com>
2021-08-20 17:20:06 -0400

    created all routes, they have been tested and are working properly

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,44 @@ class Application
     res = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/test/) 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+    if req.path == '/players' && req.get?
+       players = Player.all
+      return [
+        200, 
+      { 'Content-Type' => 'application/json' }, 
+      [ players.to_json ]
+    ]
+
+  elsif req.path == '/teams' && req.get?
+    teams = Team.all
+   return [
+     200, 
+   { 'Content-Type' => 'application/json' }, 
+   [ teams.to_json ]
+ ]
+
+elsif req.path.match(/teams/) && req.get?
+  id = req.path.split('/')[2]
+  team = Team.find_by(id: id)
+  if team
+    players = team.players
+    team_res = {
+      name: team.name,
+      location: team.location,
+      players: players
+    }
+ return [
+   200, 
+ { 'Content-Type' => 'application/json' }, 
+ [ team_res.to_json ]
+  ]
+else
+  return  [
+    204,
+    { 'Content-Type' => 'application/json' }, 
+    [ { error: 'team not found' }.to_json ]
+  ]
+end  
 
     else
       res.write "Path Not Found"
